9f4ab9c82a3856ffda0fea02e0870f21eec482ee
Matthew Chaleski <chaleski76@gmail.com>
2021-02-22 10:46:04 -0500

    new changes

--- authorization_code/public/search.js
+++ authorization_code/public/search.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const access_token = localStorage.getItem('access_token')
-console.log(access_token)
-    if (!access_token) {
-        alert('Please Login again');
-        return
-    } else {
-        //Start access to Spotify API
-        $.ajax({
-            url: 'https://api.spotify.com/v1/search?q=Foo%20Fighters&type=artist',
-            headers: {
-                'Authorization': 'Bearer ' + access_token
-            },
-            success: function (response) {
-                console.log(response)
-                // response contains search data
-            }
-        });
-    }
 
+var curOffset = 0;
+var limitPerPage = 10;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("form-control").addEventListener("click", function(e){
+        e.preventDefault()
+    const access_token = localStorage.getItem('access_token')
+    console.log(access_token)
+        if (!access_token) {
+            alert('Please Login again');
+            return
+        } else {
+            //Start access to Spotify API
+            $.ajax({
+                url: 'https://api.spotify.com/v1/search',
+                headers: {
+                    'Authorization': 'Bearer ' + access_token
+                },
+                success: function (response) {
+                    console.log(response)
+                    // response contains search data
+                }
+            });
+        }
+
+    })
 })
+
+
+
